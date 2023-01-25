f201bdfcdfd67cf623a03d73456bf79072e0b612
Kaitlin Stouffer <kstouff4@localhost.localdomain>
2023-01-25 07:08:10 -0500

    added script for testing subsampling functions

---
+++ bashScripts/testSubSample.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cd /cis/home/kstouff4/Documents/MeshRegistration/Scripts-KMS/approxCode
+
+Xfile="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/XSplits/Sub_0200-0300_XnuX._0.npz"
+sigma=0.1
+savename="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/SamplingExperiments/Sub_0200-0300_0"
+delta=0.1
+
+python3 -c "import subsampleFunctions as sf; sf.makeSubsample('$Xfile',$sigma,'$savename',xtype='discrete',ztype='semi-discrete',overhead=$delta,maxV=673,C=1.2); quit()"
+
+Xfile="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/XSplits/Sub_0200-0300_XnuX._1.npz"
+sigma=0.1
+savename="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/SamplingExperiments/Sub_0200-0300_1"
+
+python3 -c "import subsampleFunctions as sf; sf.makeSubsample('$Xfile',$sigma,'$savename',xtype='discrete',ztype='semi-discrete',overhead=$delta,maxV=673,C=1.2); quit()"
+
+Xfile="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/XSplits/Sub_0200-0300_XnuX._2.npz"
+sigma=0.1
+savename="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/SamplingExperiments/Sub_0200-0300_2"
+
+python3 -c "import subsampleFunctions as sf; sf.makeSubsample('$Xfile',$sigma,'$savename',xtype='discrete',ztype='semi-discrete',overhead=$delta,maxV=673,C=1.2); quit()"
+
+Xfile="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/XSplits/Sub_0200-0300_XnuX._3.npz"
+sigma=0.1
+savename="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/SamplingExperiments/Sub_0200-0300_3"
+
+python3 -c "import subsampleFunctions as sf; sf.makeSubsample('$Xfile',$sigma,'$savename',xtype='discrete',ztype='semi-discrete',overhead=$delta,maxV=673,C=1.2); quit()"
