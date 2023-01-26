cc4763d6b96955b0bb8972fff7d59a3bad164549
Kaitlin Stouffer <kstouff4@localhost.localdomain>
2023-01-26 07:22:52 -0500

    mRNA sampling function add and retesting pure vs mixed

--- approxCode/subsampleFunctions.py
+++ approxCode/subsampleFunctions.py
@@ -2,6 +2,7 @@ import numpy as np
 from sys import path as sys_path
 sys_path.append('/cis/home/kstouff4/Documents/SurfaceTools/')
 import vtkFunctions as vtf
+import pandas as pd
 
 ####################################################################
 # File Description
@@ -53,7 +54,7 @@ def reverseOneHot(nu_Z,indsToKeep,maxVal=673):
     nnuZ[:,indsToKeep] = nu_Z
     return nnuZ
 
- def makeSubsampleFun(X, nu_X, C=1.2,dis=True, maxV=673):
+def makeSubsampleFun(X, nu_X, sig, C=1.2,dis=True, maxV=673):
     # Input:
     # X is the initial data
     # N is the number of random subsample
@@ -84,7 +85,7 @@ def addOverhead(Xfile,Zfile,overhead=0.1,maxV=673):
     np.savez(Zfile.replace('.npz','_plus' + str(overhead) + '.npz'),Z=Z,nu_Z=nu_Z)
     return
 
-def makeSubsample(Xfile,sigma,savename,xtype='discrete',ztype='semi-discrete',overhead=0.1,maxV=673,C=1.2):
+def makeSubsample(Xfile,sig,savename,xtype='discrete',ztype='semi-discrete',overhead=0.1,maxV=673,C=1.2):
     '''
     Types of sampling:
         - xtype = semi-discrete, then z type = semi-discrete
@@ -96,7 +97,7 @@ def makeSubsample(Xfile,sigma,savename,xtype='discrete',ztype='semi-discrete',ov
     X=xinfo['X']
     nu_X = xinfo['nu_X']
     if (xtype == 'discrete'):
-        Z, nu_Z = makeSubsampleFun(X,nu_X,C=C,dis=True,maxV=maxV)
+        Z, nu_Z = makeSubsampleFun(X,nu_X,sig,C=C,dis=True,maxV=maxV)
         # write original Z 
         maxInd = np.argmax(nu_Z,axis=-1)+1
         vtf.writeVTK(Z,[maxInd,np.sum(nu_Z,axis=-1)],['MAX_VAL_NU','TOTAL_MASS'],savename+'_originalZnu_ZwC' + str(C) + '_sig' + str(sig) + '_discrete.vtk',polyData=None)
@@ -107,13 +108,87 @@ def makeSubsample(Xfile,sigma,savename,xtype='discrete',ztype='semi-discrete',ov
             nu_Z = reverseOneHot(nu_Z,indsToKeep,maxV)
             np.savez(savename+'_originalZnu_ZwC' + str(C) + '_sig' + str(sig) + '_semidiscrete_plus' + str(overhead) + '.npz',Z=Z, nu_Z=nu_Z)
     elif (xtype == 'semi-discrete'):
-        Z, nu_Z = makeSubsampleFun(X,nu_X,C=C,dis=False,maxV=maxV)
+        Z, nu_Z = makeSubsampleFun(X,nu_X,sig,C=C,dis=False,maxV=maxV)
         # write original Z 
         maxInd = np.argmax(nu_Z,axis=-1)+1
         vtf.writeVTK(Z,[maxInd,np.sum(nu_Z,axis=-1)],['MAX_VAL_NU','TOTAL_MASS'],savename+'_originalZnu_ZwC' + str(C) + '_sig' + str(sig) + '_semidiscrete.vtk',polyData=None)
         np.savez(savename+'_originalZnu_ZwC' + str(C) + '_sig' + str(sig) + '_semidiscrete.npz',Z=Z, nu_Z=nu_Z)
     
     return
+
+def makeSubsampleStratified(Xfile,coordName,nuName,sig,savename,xtype='discrete',ztype='semi-discrete',overhead=0.1,maxV=702,C=1.2,dim=2):
+    '''
+    Sampling of same types as in makeSubsample, but with weighting geographically in subset
+    
+    Algorithm:
+        - Divide bounding box into squares (cubes) of area / volume sigma^dim
+        - Select np.round(C*np.log(counts + 1)) particles from each square (cube) based on counts in the cube 
+        
+    coordName = name of variable storing coordinates (assume N x 2/3)
+    nuName = name of variable storing feature value (assume N x 1)
+    
+    '''
+    info = np.load(Xfile)
+    coords = info[coordName]
+    if coords.shape[-1] > dim:
+        coords = coords[:,0:dim]
+    
+    nuX = np.squeeze(info[nuName])
+    
+    # divide into cubes of size sig
+    if (dim == 2):
+        coords_labels = np.floor((coords - np.floor(np.min(coords,axis=0)))/sig).astype(int) # minimum number of cubes in x and y 
+        totCubes = (np.max(coords_labels[:,0])+1)*(np.max(coords_labels[:,1])+1)
+        #xC = np.arange(np.max(coords_labels[:,0])+1)*sig + np.floor(np.min(coords[:,0])) + sig/2.0
+        #yC = np.arange(np.max(coords_labels[:,1])+1)*sig + np.floor(np.min(coords[:,1])) + sig/2.0
+        #XC,YC = np.meshgrid(xC,yC,indexing='ij')
+        #cubes_centroids = np.stack((XC,YC),axis=-1)
+        #cubes_indices = np.reshape(np.arange(totCubes),(cubes_centroids.shape[0],cubes_centroids.shape[1]))
+        #coords_labels_tot = cubes_indices[coords_labels[:,0],coords_labels[:,1]]
+    elif (dim == 3):
+        coords_labels = np.floor((coords - np.floor(np.min(coords,axis=0)))/sig).astype(int) # minimum number of cubes in x and y 
+        totCubes = (np.max(coords_labels[:,0])+1)*(np.max(coords_labels[:,1])+1) + (np.max(coords_labels[:,2]) + 1)
+        #xC = np.arange(np.max(coords_labels[:,0])+1)*sig + np.floor(np.min(coords[:,0])) + sig/2.0
+        #yC = np.arange(np.max(coords_labels[:,1])+1)*sig + np.floor(np.min(coords[:,1])) + sig/2.0
+        #zC = np.arange(np.max(coords_labels[:,2])+1)*sig + np.floor(np.max(coords[:,2])) + sig/2.0
+        #XC,YC,ZC = np.meshgrid(xC,yC,zC,indexing='ij')
+        #cubes_centroids = np.stack((XC,YC,ZC),axis=-1)
+        #cubes_indices = np.reshape(np.arange(totCubes),(cubes_centroids.shape[0],cubes_centroids.shape[1],cubes_centroids.shape[2]))
+        #coords_labels_tot = cubes_indices[coords_labels[:,0],coords_labels[:,1],coords_labels[:,2]]
+    
+    # coords_labels_tot gives the index of the cube for each of original measurements
+    df = pd.DataFrame()
+    df['x'] = coords[:,0]
+    df['y'] = coords[:,1]
+    df['nu'] = nuX
+    df['cX'] = coords_labels[:,0]
+    df['cY'] = coords_labels[:,1]
+    if (dim == 3):
+        df['z'] = coords[:,2]
+        df['cZ'] = coords_labels[:,2]
+        dff = df.groupby(['cX','cY','cZ']).sample(n=1) # select 1 from each group (could do frac=0.1 to get 10% total in each group)
+        Z = dff['x','y','z'].to_numpy()
+        nuZ = dff['nu'].to_numpy()
+    elif (dim == 2):
+        dff = df.groupby(['cX','cY']).sample(n=1)
+        Z = dff['x','y'].to_numpy()
+        nuZ = dff['nu'].to_numpy()
+    nu_Z = makeOneHot(nuZ,maxVal=maxV)*coords.shape[0]/nuZ.shape[0] # weigh points with appropriate mass and make large array
+    
+    maxInd = np.argmax(nu_Z,axis=-1)+1
+    vtf.writeVTK(Z,[maxInd,np.sum(nu_Z,axis=-1)],['MAX_VAL_NU','TOTAL_MASS'],savename+'_originalZnu_ZwC' + str(C) + '_sig' + str(sig) + '_discrete.vtk',polyData=None)
+    np.savez(savename+'_originalZnu_ZwC' + str(C) + '_sig' + str(sig) + '_discrete.npz',Z=Z, nu_Z=nu_Z)
+    if (ztype == 'semi-discrete'):
+        nnu_X,nnu_Z,indsToKeep = oneHot(nuX,nu_Z)
+        nu_Z = nnu_Z + overhead
+        nu_Z = reverseOneHot(nu_Z,indsToKeep,maxV)
+        np.savez(savename+'_originalZnu_ZwC' + str(C) + '_sig' + str(sig) + '_semidiscrete_plus' + str(overhead) + '.npz',Z=Z, nu_Z=nu_Z)
+    
+    return
+
+
+        
+
 
         
 
--- bashScripts/testSubSample.sh
+++ bashScripts/testSubSample.sh
@@ -6,22 +6,22 @@ sigma=0.1
 savename="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/SamplingExperiments/Sub_0200-0300_0"
 delta=0.1
 
-python3 -c "import subsampleFunctions as sf; sf.makeSubsample('$Xfile',$sigma,'$savename',xtype='discrete',ztype='semi-discrete',overhead=$delta,maxV=673,C=1.2); quit()"
+python3 -c "import subsampleFunctions as sf; sf.makeSubsample('$Xfile',$sigma,'$savename',xtype='discrete',ztype='semi-discrete',overhead=$delta,maxV=673,C=0.6); quit()"
 
 Xfile="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/XSplits/Sub_0200-0300_XnuX._1.npz"
 sigma=0.1
 savename="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/SamplingExperiments/Sub_0200-0300_1"
 
-python3 -c "import subsampleFunctions as sf; sf.makeSubsample('$Xfile',$sigma,'$savename',xtype='discrete',ztype='semi-discrete',overhead=$delta,maxV=673,C=1.2); quit()"
+python3 -c "import subsampleFunctions as sf; sf.makeSubsample('$Xfile',$sigma,'$savename',xtype='discrete',ztype='semi-discrete',overhead=$delta,maxV=673,C=0.6); quit()"
 
 Xfile="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/XSplits/Sub_0200-0300_XnuX._2.npz"
 sigma=0.1
 savename="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/SamplingExperiments/Sub_0200-0300_2"
 
-python3 -c "import subsampleFunctions as sf; sf.makeSubsample('$Xfile',$sigma,'$savename',xtype='discrete',ztype='semi-discrete',overhead=$delta,maxV=673,C=1.2); quit()"
+python3 -c "import subsampleFunctions as sf; sf.makeSubsample('$Xfile',$sigma,'$savename',xtype='discrete',ztype='semi-discrete',overhead=$delta,maxV=673,C=0.6); quit()"
 
 Xfile="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/XSplits/Sub_0200-0300_XnuX._3.npz"
 sigma=0.1
 savename="/cis/home/kstouff4/Documents/MeshRegistration/Particles/AllenAtlas10um/SamplingExperiments/Sub_0200-0300_3"
 
-python3 -c "import subsampleFunctions as sf; sf.makeSubsample('$Xfile',$sigma,'$savename',xtype='discrete',ztype='semi-discrete',overhead=$delta,maxV=673,C=1.2); quit()"
+python3 -c "import subsampleFunctions as sf; sf.makeSubsample('$Xfile',$sigma,'$savename',xtype='discrete',ztype='semi-discrete',overhead=$delta,maxV=673,C=0.6); quit()"
